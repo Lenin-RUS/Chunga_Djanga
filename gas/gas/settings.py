7c6aeb653720ddf157b756fb294e3795262e1ea0
Lenin <S.Lanin@gmail.com>
2020-03-04 20:16:30 +0300

    ДЗ_22

--- gas/gas/settings.py
+++ gas/gas/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
